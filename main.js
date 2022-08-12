4fd1936c4622069442d230995a2dfc15bb92b538
4015 <4015.dev@pm.me>
2022-08-12 20:15:12 +0100

    fix mixing operations, add clear button

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var i = 0;
 const keyNumbers = document.querySelectorAll(".dig");
 const keyOperators = document.querySelectorAll(".operator");
 const keys = document.querySelectorAll(".keys>div>*");
-// console.log({keys});
+console.log({ keys });
 
 var strOperations = "0";
 let arrOperations;
 
-function operate1(op, num1, num2) {
+function operate(op, num1, num2) {
   let a = Number(num1);
   let b = Number(num2);
 
@@ -20,17 +20,6 @@ function operate1(op, num1, num2) {
       return a / b;
     case "%":
       return a % b;
-
-    default:
-      break;
-  }
-}
-
-function operate2(op, num1, num2) {
-  let a = Number(num1);
-  let b = Number(num2);
-
-  switch (op) {
     case "+":
       return a + b;
     case "-":
@@ -41,53 +30,77 @@ function operate2(op, num1, num2) {
   }
 }
 
-function operate(arr) {
-  let res = arr;
-  let operator = ["*", "/", "%", "+", "-"];
+let operation = [];
 
-  let iteration = 0;
-  // console.log(
-  //   `c1 includes ${operator[0]}`,
-  //   res.indexOf(operator[0]) > 0
-  // );
+function indexOfOperator(operation, operatorsString) {
+  let operator = operatorsString.split(" ");
+  let operatorIndex = [];
 
-  while (res.indexOf(operator[0]) > 0 && res.length > res.indexOf(operator[0]) + 1) {
-    console.log("#", iteration++);
-    console.log(res);
-    let op = res.indexOf(operator[0]);
-    if (op > 0 && res.length > op + 1) {
-      // console.log(arr[op - 1], arr[op], arr[op + 1]);
-      let opRes = operate1(res[op], res[op - 1], res[op + 1]);
-      console.log(opRes);
+  // return ops.indexOf("*");
+  for (let i = 0; i < operator.length; i++) {
+    let ndx = operation.indexOf(operator[i]);
+    if (ndx > -1) operatorIndex.push(ndx);
+  }
+  // console.log('operatorIndex :>> ', operatorIndex);
+  let res = operatorIndex.length > 0 ? Math.min(...operatorIndex) : -1;
+  // console.log('res :>> ', res);
+  return res;
+}
 
-      res.splice(op - 1, 3, opRes);
-      console.log(res);
-    }
+function operateAll(ops) {
+  const initialLength = ops.length;
+  let res = ops;
+  console.log("====");
+
+  const operatorsStr1 = "* / %";
+  let op1 = indexOfOperator(res, operatorsStr1);
+  const operatorsStr2 = "+ -";
+  let op2 = indexOfOperator(res, operatorsStr2);
+
+
+  while (res.length > 2 && (op1 > -1 || op2 > -1)) {
+    console.log("#"+ (initialLength - res.length)/2+" iteration");
+    console.log(`index of ${operatorsStr1.split(" ")}  :>> `, op1);
+    console.log(`index of ${operatorsStr2.split(" ")}  :>> `, op2);
+    let opIndex = op1 > -1 ? op1 : op2;
+    console.log("opIndex :>> ", opIndex);
+    let op = res[opIndex];
+    let n1 = res[opIndex - 1];
+    let n2 = res[opIndex + 1];
+    console.log("operate(op, n1, n2) :>> ", [op, n1, n2]);
+    opResult = operate(op, n1, n2);
+    console.log("opResult :>> ", opResult);
+    res.splice(opIndex - 1, 3, opResult);
+    console.log("res :>> ", res);
+
+    op1 = indexOfOperator(res, operatorsStr1);
+    op2 = indexOfOperator(res, operatorsStr2);
   }
 
-  console.log("===");
-  console.log(res);
-  console.log();
-  return res[0];
+  console.log("====");
 }
 
 keys.forEach((element) => {
   element.addEventListener("click", (e) => {
     let tmp = element.id;
-
-    if (/^[\+\-\*\/\%]+$/.test(tmp)) {
+    let clear = false;
+    if (tmp === "C") {
+      clear = true;
+      strOperations = "0";
+      i = 0;
+      display();
+    } else if (/^[\+\-\*\/\%]+$/.test(tmp)) {
       // if entering operands after pressing =, continue using the last result
-      if (i==-1) i=0;
-
+      if (i == -1) i = 0;
       ops[++i] = tmp;
       ++i;
       strOperations = "0";
     } else if (tmp != "=") {
       // if entering numbers after pressing =, start a new operation
-      if (i==-1) {
+      if (i == -1) {
         strOperations = "0";
-        ops[0]=0
-        i=0;
+        ops[0] = 0;
+        i = 0;
       }
       if (strOperations == "0") {
         strOperations = tmp;
@@ -96,21 +109,25 @@ keys.forEach((element) => {
       }
       ops[i] = strOperations;
     } else {
-      strOperations = operate(ops);
-      i=-1;
+      strOperations = operateAll(ops);
+      i = -1;
     }
 
-    console.log('ops :>> ', ops);
-    console.log('strOperations :>> ', strOperations);
-
-    display(ops,strOperations)
+    console.log("ops :>> ", ops);
+    // console.log("strOperations :>> ", strOperations);
+    if (!clear) {
+      display(ops, strOperations);
+    }
   });
 });
 
-function display(operations,result){
-  const operationScreen = document.querySelector("#operation")
-  const resultScreen = document.querySelector("#result")
+function display(_operations, _result) {
+  const operationScreen = document.querySelector("#operation");
+  const resultScreen = document.querySelector("#result");
 
-  operationScreen.textContent = operations.join('')
-  resultScreen.textContent = result
+  operationScreen.textContent =
+    _operations != undefined && Array.isArray(_operations)
+      ? _operations.join("")
+      : "";
+  resultScreen.textContent = _result ? _result : "0";
 }
